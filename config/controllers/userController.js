af162b804f9cd326940ae8ce2f4db9b3a6f379c6
jennifer-glitch <toborerichard15@gmail.com>
2024-10-29 13:17:50 +0100

    Add files via upload

---
+++ config/controllers/userController.js
@@ -0,0 +1,10 @@
+
+
+// API controller function to manage clerk user with database
+//http://localhost:4000/api/user/webhooks
+
+// import { request, response } from "express";
+
+const clerkwebhooks = async (req, res) =>{
+    
+}
---
+++ server.js
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+import express, { request, response } from 'express'
+import cors from 'cors'
+import connectDB from './config/mongodb.js'
+
+
+//App config
+const PORT = process.env.PORT || 4000
+const app = express()
+await connectDB()
+
+//initialize middlewares
+app.use(express.json())
+app.use(cors())
+
+//API route
+app.get('/', (request,response)=>response.send("API working"))
+
+app.listen(PORT, ()=>console.log("Server running on port" +PORT))
